0f098bf35ee8848eff979b4c56fe5aa22aaf201b
Prostotavov <Romasenkevich@bk.ru>
2022-08-29 21:39:38 +0300

    End

--- Defend It/PresentationLayer/Flows/BattleFlow/Modules/WinBattle/View/Views/WinBattleView.swift
+++ Defend It/PresentationLayer/Flows/BattleFlow/Modules/WinBattle/View/Views/WinBattleView.swift
@@ -11,7 +11,7 @@ protocol WinBattleViewDelegate: AnyObject {
     func goToHomePagePressed()
 }
 
-class WinBattleView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
+class WinBattleView: UIView {
     
     let startButtonHeight: CGFloat = 50
     let startButtonWidth: CGFloat = 300
@@ -23,15 +23,7 @@ class WinBattleView: UIView, UICollectionViewDataSource, UICollectionViewDelegat
     var statusLabel = UILabel()
     var rewardLabel = UILabel()
     
-    
-    let collectionViewHeight: CGFloat = 200
-    let collectionViewWidth: CGFloat = 300
-    var collectionView: UICollectionView?
-    
-    let testArrayOfRewardItems: [(ImageNames, String)] = [
-        (ImageNames.blade, "1"), (ImageNames.arch, "2"), (ImageNames.hammer, "1"),
-        (ImageNames.diamond, "10"), (ImageNames.money, "175"), (ImageNames.flask, "7")
-    ]
+    let rewardCollectionView = RewardsCollectionView()
     
     weak var delegate: WinBattleViewDelegate!
     
@@ -50,44 +42,16 @@ class WinBattleView: UIView, UICollectionViewDataSource, UICollectionViewDelegat
     }
     
     func setCollectionView() {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.itemSize = CGSize(width: (collectionViewWidth / 5) - 1,
-                                 height: (collectionViewWidth / 5) - 1)
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        guard let collectionView = collectionView else {return}
-        collectionView.register(EquipmentCellImp.self, forCellWithReuseIdentifier: EquipmentCellImp.identifier)
-        collectionView.contentInset = UIEdgeInsets(top: 7.0, left: 7.0, bottom: 7, right: 7.0)
+        self.addSubview(rewardCollectionView)
         
-        self.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        rewardCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            collectionView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            collectionView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 100),
-            collectionView.heightAnchor.constraint(equalToConstant: collectionViewHeight),
-            collectionView.widthAnchor.constraint(equalToConstant: collectionViewWidth)
+            rewardCollectionView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            rewardCollectionView.topAnchor.constraint(equalTo: rewardLabel.bottomAnchor, constant: 30)
         ])
-        
-        collectionView.backgroundColor = .yellow
-        
-        collectionView.dataSource = self
-        collectionView.delegate = self
     }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        testArrayOfRewardItems.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EquipmentCellImp.identifier, for: indexPath) as! EquipmentCell
-        
-        let imageName = testArrayOfRewardItems[indexPath.row].0
-        let text = testArrayOfRewardItems[indexPath.row].1
-        
-        cell.configure(image: imageName, text: text)
-        return cell
-    }
+
     
     func setStatusLabel() {
         
